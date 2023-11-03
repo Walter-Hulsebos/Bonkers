8792187287797ce95ee1396caf121004b761b22c
GaronBos <101883403+GaronBos@users.noreply.github.com>
2023-11-03 16:06:41 +0100

    penis

--- Assets/03_Scripts/06_Data/PlayerData.cs
+++ Assets/03_Scripts/06_Data/PlayerData.cs
@@ -11,12 +11,12 @@ public class PlayerData : ScriptableObject
     public float OrientSharpness = 20f;
 
     [Header("Jump State")]
-    public float jumpVelocity = 15f;
-    public int amountOfJumps = 1;
+    //public float jumpVelocity = 15f;
+   //public int amountOfJumps = 1;
 
     [Header("In Air State")]
-    public float coyoteTime = 0.02f;
-    public float variableJumpHeightMultiplier = 0.5f;
+    //public float coyoteTime = 0.02f;
+   // public float variableJumpHeightMultiplier = 0.5f;
 
     [Header("Druid Rat Combat")]
     public GameObject ratPrefab;
