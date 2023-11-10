c9fcc287c13b3b7342f113377041213f1ce4f88b
TotiVasilev <todorvasilev0144@gmail.com>
2023-11-11 00:29:40 +0100

    Main menu implemented

---
+++ Assets/03_Scripts/01_Lobby/MainMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+namespace Bonkers
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Image pressAnyButtonImage;
+        [SerializeField] private GameObject menu;
+        public void Start()
+        {
+           
+        }
+        public void Update()
+        {
+            if (Keyboard.current.anyKey.isPressed)
+            {
+                pressAnyButtonImage.enabled = false;
+                menu.SetActive(true);
+            }
+        }
+
+        public void NewCampaign()
+        {
+            
+        }
+
+        public void OnlinePlay()
+        {
+            SceneManager.LoadSceneAsync("Bootstrap");
+        }
+
+        public void OnfflinePlay() 
+        {
+            
+        }
+
+        public void Options()
+        {
+
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
