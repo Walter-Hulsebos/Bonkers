3088757cf4624aa810504eb5d913130fd91925a0
Walter Hulsebos <walterhulsebos@outlook.com>
2023-11-23 00:57:56 +0100

    REEEE

--- Assets/03_Scripts/01_Lobby/Networking/Server/Netcode/MatchplayNetworkServer.cs
+++ Assets/03_Scripts/01_Lobby/Networking/Server/Netcode/MatchplayNetworkServer.cs
@@ -109,7 +109,7 @@ public class MatchplayNetworkServer : IDisposable
     {
         gameHasStarted = true;
 
-        NetworkManager.Singleton.SceneManager.LoadScene(sceneName: "ImplementerTestScene", loadSceneMode: LoadSceneMode.Single);
+        NetworkManager.Singleton.SceneManager.LoadScene(sceneName: "T3_Final", loadSceneMode: LoadSceneMode.Single);
     }
 
     private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
