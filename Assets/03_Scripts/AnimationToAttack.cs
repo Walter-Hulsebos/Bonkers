0d03cf6f6dd091bfcd9dab6754b4efcb23a19af8
GaronBos <garonbos@gmail.com>
2023-11-23 12:54:51 +0100

    fixed cat woman

---
+++ Assets/03_Scripts/AnimationToAttack.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bonkers
+{
+    public class AnimationToAttack : MonoBehaviour
+    {
+        private void TriggerAttack(){}
+        private void FinishAttack(){}
+    }
+}
