323c787933c166e8226fe112b58490a5a7f15ddb
Walter Hulsebos <walterhulsebos@outlook.com>
2023-11-28 13:48:42 +0100

    Teams Finished

--- Assets/03_Scripts/99_Shared/Team.cs
+++ Assets/03_Scripts/99_Shared/Team.cs
@@ -20,7 +20,7 @@ namespace Bonkers
         public Color  Color   { get; [UsedImplicitly] private set; } = Color.white;
         [field:SerializeField]
         public Team[] Enemies { get; [UsedImplicitly] private set; } = Array.Empty<Team>();
-
+        
         #region Custom Editor
 
         #if UNITY_EDITOR
@@ -45,10 +45,22 @@ namespace Bonkers
         //     }
         // }
         [InitializeOnLoad]
-        private class TeamProjectWindowIcons
+        private sealed class TeamProjectWindowIcons
         {
+            private static Color32
+                _proColor   = new(r: 51,  g: 51,  b: 51,  a: 255),
+                _scrubColor = new(r: 190, g: 190, b: 190, a: 255);
+            
+            [PublicAPI]
+            public static ref Color32 BackgroundColor => ref EditorGUIUtility.isProSkin ? ref _proColor : ref _scrubColor;
+            
+            private static Texture2D _teamIcon = null;
+            
             static TeamProjectWindowIcons()
-            {
+            {               
+                _teamIcon = EditorGUIUtility.FindTexture(name: "Assets/03_Scripts/99_Shared/TeamIcon.png");
+                Debug.Assert(condition: _teamIcon != null, message: nameof(_teamIcon) + " is NULL!!");
+                
                 EditorApplication.projectWindowItemOnGUI += DrawProjectWindowItem;
             }
 
@@ -56,17 +68,42 @@ namespace Bonkers
             {
                 String __assetPath = AssetDatabase.GUIDToAssetPath(guid: guid);
                 
-                if(!IsTeamAsset(assetPath: __assetPath)) return;
+                if(!IsTeamAsset(assetPath: __assetPath, team: out Team __team)) return;
                 
+                DrawBackgroundRect(rect: selectionRect);
                 
+                DrawTeamIcon(rect: selectionRect, team: __team);
             }
             
-            private static Bool IsTeamAsset(String assetPath)
+            private static Bool IsTeamAsset(String assetPath, out Team team)
             {
-                if(!assetPath.EndsWith(value: ".asset", comparisonType: StringComparison.OrdinalIgnoreCase)) return false;
+                if (!assetPath.EndsWith(value: ".asset", comparisonType: StringComparison.OrdinalIgnoreCase))
+                {
+                    team = null;
+                    return false;
+                }
                 
-                Team __team = AssetDatabase.LoadAssetAtPath<Team>(assetPath: assetPath);
-                return __team != null;
+                team = AssetDatabase.LoadAssetAtPath<Team>(assetPath: assetPath);
+                return team != null;
+            }
+            
+            private static void DrawBackgroundRect(Rect rect) => EditorGUI.DrawRect(rect: rect, color: BackgroundColor);
+
+            [PublicAPI]
+            private static void DrawTeamIcon(Rect rect, Team team)
+            {
+                //EditorGUI.DrawPreviewTexture();
+                
+                Color __oldColor = GUI.color;
+                GUI.color = team.Color;
+
+                Vector2 __pos  = rect.position;
+                Vector2 __size = new(x: rect.height, y: rect.height);
+			
+                GUI.DrawTexture(position: new Rect(position: __pos, size: __size), image: _teamIcon);
+                //GUI.DrawTexture(position: rect, image: _teamIcon);
+                
+                GUI.color = __oldColor;
             }
         }
         #endif
