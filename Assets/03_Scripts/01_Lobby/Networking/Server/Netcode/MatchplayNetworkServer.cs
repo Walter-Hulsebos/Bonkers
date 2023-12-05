9c7131ff67f6ddef1b2dc93a9e26546427501158
GaronBos <101883403+GaronBos@users.noreply.github.com>
2023-12-05 21:16:51 +0100

    Spawn pos of the player should not be random

--- Assets/03_Scripts/01_Lobby/Networking/Server/Netcode/MatchplayNetworkServer.cs
+++ Assets/03_Scripts/01_Lobby/Networking/Server/Netcode/MatchplayNetworkServer.cs
@@ -109,7 +109,7 @@ public class MatchplayNetworkServer : IDisposable
     {
         gameHasStarted = true;
 
-        NetworkManager.Singleton.SceneManager.LoadScene(sceneName: "T3_Final", loadSceneMode: LoadSceneMode.Single);
+        NetworkManager.Singleton.SceneManager.LoadScene(sceneName: "Arena 1", loadSceneMode: LoadSceneMode.Single);
     }
 
     private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
