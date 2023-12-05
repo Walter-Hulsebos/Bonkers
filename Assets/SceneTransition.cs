d994381d3532053643af491dae3e6a575952aac0
bbobbii <bobymanti@gmail.com>
2023-12-05 16:23:32 +0100

    .

--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -6,16 +6,8 @@ namespace Bonkers
 {
     public class SceneTransition : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+
+
     }
 }
