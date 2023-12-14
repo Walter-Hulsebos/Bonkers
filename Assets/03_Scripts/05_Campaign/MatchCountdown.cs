77b6bb129344b758ef26890d13bedfe3ff8c29e2
pjboer1 <boer.pim77@gmail.com>
2023-12-14 16:16:51 +0100

    - Added some more comment for clarity

--- Assets/03_Scripts/05_Campaign/MatchCountdown.cs
+++ Assets/03_Scripts/05_Campaign/MatchCountdown.cs
@@ -27,7 +27,7 @@ namespace Bonkers
         }
 
         /// <summary>
-        /// Get the remaining time of the match countdown timer.
+        /// Get the remaining time of the match countdown timer in raw seconds.
         /// Please dont call this in Update (Rather every second or half a second)
         /// </summary>
         public float GetRemainingTime()
@@ -39,7 +39,7 @@ namespace Bonkers
         /// Start the countdown Timer
         /// </summary>
         /// <param name="_amountInSeconds"></param>
-        public void StartMatchCountdown(int _amountInSeconds = 600)
+        public void StartMatchCountdown(int _amountInSeconds = 300)
         {
             matchCountdownTimer.SetTimer(_amountInSeconds);
         }
@@ -66,19 +66,17 @@ namespace Bonkers
             return matchCountdownTimer.TimerProgress();
         }
 
-
-
         /// <summary>
         /// Method called when the timer is done.
         /// </summary>
         public void StopMatchCountdown()
         {
-
+            //Add Connection here
         }
 
         /// <summary>
         /// Method call for pausing the match count down timer.
-        /// </summary>01
+        /// </summary>
         /// <param name="_value"></param>
         public void PauseTimer(bool _value)
         {
