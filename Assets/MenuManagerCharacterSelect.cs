32c404aa9690e295f27dd36a704c1a0024033027
bbobbii <bobymanti@gmail.com>
2023-12-19 16:11:06 +0100

    Added map select
    needs functionality

--- Assets/MenuManagerCharacterSelect.cs
+++ Assets/MenuManagerCharacterSelect.cs
@@ -11,37 +11,51 @@ namespace Bonkers
     public class MenuManagerCharacterSelect : MonoBehaviour
     {
         [SerializeField] private InputActionReference backorClose;
+        [SerializeField] private InputActionReference accept;
+        [SerializeField] private SelectButtonFirst selectButtonFirst;
         [SerializeField] private GameObject quitPopup;
         private bool popupActive;
 
         private void OnEnable()
         {
             backorClose.action.performed += CheckLeaveLobby;
+            //accept.action.performed += ReturnToStatingScene;
             backorClose.action.Enable();
         }
         private void OnDisable()
         {
             backorClose.action.performed -= CheckLeaveLobby;
+            //accept.action.performed -= ReturnToStatingScene;
             backorClose.action.Disable();
         }
 
         private void Update()
         {
-            if(!popupActive && Input.GetKeyDown(KeyCode.E))
+            if(!popupActive && Input.GetKeyDown(KeyCode.Return))
             {
                 SceneManager.LoadSceneAsync("StartMenu");
             }
         }
 
+        //private void ReturnToStatingScene(InputAction.CallbackContext context)
+        //{
+        //    if (!popupActive)
+        //    {
+        //        SceneManager.LoadSceneAsync("StartMenu");
+        //    }
+        //}
+
         private void CheckLeaveLobby(InputAction.CallbackContext context)
         {
             if (popupActive)
             {
+                EventSystem.current.SetSelectedGameObject(null);
                 popupActive = false;
                 quitPopup.SetActive(true);
             }
             else
             {
+                selectButtonFirst.SetSelectedFirst();
                 popupActive = true;
                 quitPopup.SetActive(false);
             }           
